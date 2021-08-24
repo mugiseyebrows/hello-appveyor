0249ecd25829bf282e3474022acd57c5c1bf2271
Doronin Stanislav <mugisbrows@gmail.com>
2021-08-24 18:14:04 +0300

    1.3.8

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,5 @@
 #include <QTest>
 
 #include "testqstring.h"
-#include "testfoo.h"
 
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-
-    int t1 = QTest::qExec(new TestFoo, argc, argv);
-    int t2 = QTest::qExec(new TestQString, argc, argv);
-
-    return 0;
-}
+QTEST_MAIN(TestQString)
