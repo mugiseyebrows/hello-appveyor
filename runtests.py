471f63364025dd2182fe776181a278fd63d78455
Doronin Stanislav <mugisbrows@gmail.com>
2021-10-15 16:16:26 +0300

    1.3.29

--- runtests.py
+++ runtests.py
@@ -1,12 +1,14 @@
 import argparse
-
+import requests
+from io import BytesIO
 
 parser = argparse.ArgumentParser()
-parser.add_argument('job-id')
+parser.add_argument('APPVEYOR_JOB_ID')
 
 args = parser.parse_args()
 
-print('job-id', args.job_id)
+#print(args)
+#print('APPVEYOR_JOB_ID', args.APPVEYOR_JOB_ID)
 
 import subprocess
 res = subprocess.Popen(['tests\\release\\tests', '-xunitxml'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -14,5 +16,10 @@ stdout, stderr = res.communicate()
 fail = False
 if res.returncode == 1:
     open('.fail', 'w').close()
+    fail = True
+"""
 with open('TEST1.xml','wb') as f:
     f.write(stdout)
+"""
+resp = requests.post('https://ci.appveyor.com/api/testresults/junit/{}'.format(args.APPVEYOR_JOB_ID), files={'file':stdout})
+print(resp)
